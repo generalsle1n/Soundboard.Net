59b93fe55c96509e8477031e96fd160715175a8e
generalsle1n <niels.schuler@web.de>
2024-01-06 20:06:15 +0100

    Init

---
+++ Soundboard.Net/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Soundboard.Net.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Soundboard.Net/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Soundboard.Net.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
