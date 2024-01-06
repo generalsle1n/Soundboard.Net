f9883814f19adb8c81a495d024e32f4ef7e99820
generalsle1n <niels.schuler@web.de>
2024-01-06 22:39:08 +0100

    Added Soundplayer

---
+++ Soundboard.Net.Manager/Model/Sound.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soundboard.Net.Manager.Model
+{
+	public class Sound
+	{
+		public string Name { get; set; }
+		public string Length { get; set; }
+		public string AbsolutePath { get; set; }
+	}
+}
--- Soundboard.Net.Manager/SoundManager.cs
+++ Soundboard.Net.Manager/SoundManager.cs
@@ -1,7 +1,54 @@
-﻿namespace Soundboard.Net.Manager
+﻿using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using Soundboard.Net.Manager.Model;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace Soundboard.Net.Manager
 {
 	public class SoundManager
 	{
+		private const string _soundFolderName = "Sounds";
+		private List<Sound> _allSounds = new List<Sound>();
+		private WaveOutEvent _output;
+		private MixingSampleProvider _mixer;
+		public SoundManager()
+		{
+			string BinaryPath = Assembly.GetExecutingAssembly().Location;
+			string SoundFolderPath = Path.Combine(Path.GetDirectoryName(BinaryPath), _soundFolderName);
+			string[] RawFiles = Directory.GetFiles(SoundFolderPath);
 
+			foreach(string SingleFile in RawFiles)
+			{
+				_allSounds.Add(new Sound()
+				{
+					Name = Path.GetFileNameWithoutExtension(SingleFile),
+					AbsolutePath = SingleFile
+				});
+			}
+
+			InitAudioEngine();
+		}
+		public IEnumerable<Sound> GetAllSounds()
+		{
+			return _allSounds;
+		}
+
+		private void InitAudioEngine()
+		{
+			_output = new WaveOutEvent();
+			_mixer = new MixingSampleProvider(WaveFormat.CreateIeeeFloatWaveFormat(44100, 2));
+			_mixer.ReadFully = true;
+			_output.Init(_mixer);
+			_output.Play();
+		}
+
+		public async Task PlaySound(Sound Sound)
+		{
+			using(AudioFileReader AudioReader = new AudioFileReader(Sound.AbsolutePath))
+			{
+				_mixer.AddMixerInput(AudioReader.ToWaveProvider());
+			}
+		}
 	}
 }
