c0a21015c1c4e79cf7eaba34486d85f8e0674eff
generalsle1n <niels.schuler@web.de>
2024-01-06 22:40:00 +0100

    Added automatic Button creation

--- Soundboard.Net/Views/MainView.axaml.cs
+++ Soundboard.Net/Views/MainView.axaml.cs
@@ -1,11 +1,30 @@
 ﻿using Avalonia.Controls;
+using DynamicData;
+using ReactiveUI;
+using Soundboard.Net.Manager;
+using Soundboard.Net.Manager.Model;
 
 namespace Soundboard.Net.Views;
 
 public partial class MainView : UserControl
 {
     public MainView()
-    {
-        InitializeComponent();
+    {   
+		InitializeComponent();
+        SoundManager Manager = new SoundManager();
+        foreach(Sound SingleSound in Manager.GetAllSounds())
+        {
+            SoundAction.Children.Add(new Button()
+            {
+                Content = SingleSound.Name,
+                Command = ReactiveCommand.Create(async () =>
+                {
+					Manager.PlaySound(SingleSound);
+                })
+            });
+
+		}
+        
+        
     }
 }
