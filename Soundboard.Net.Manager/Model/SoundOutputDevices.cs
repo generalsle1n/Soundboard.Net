31187b4bb3644cfe15930c855b2247e735627214
generalsle1n <niels.schuler@web.de>
2024-01-07 10:46:55 +0100

    Added New Output Device Model

---
+++ Soundboard.Net.Manager/Model/SoundOutputDevices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soundboard.Net.Manager.Model
+{
+	public class SoundOutputDevices
+	{
+	}
+}
